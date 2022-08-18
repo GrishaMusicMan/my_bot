3b7b37182693ca91ea00d99eecaa5e2947186a80
Grisha <parfen20@rambler.ru>
2022-08-18 13:14:32 +0200

    add photo

--- new_bot.py
+++ new_bot.py
@@ -7,4 +7,20 @@ def start(message):
     mess = f'Здарова {message.from_user.first_name}'
     bot.send_message(message.chat.id, mess)
 
+@bot.message_handler()
+def get_message(message):
+    if message.text == "Привет":
+        bot.send_message(message.chat.id, 'И тебе Привет!')
+    elif message.text == "id":
+        bot.send_message(message.chat.id, f'Твой id: {message.from_user.id}')
+    elif message.text == "photo":
+        photo = open('Mybot.png', 'rb')
+        bot.send_photo(message.chat.id, photo)
+
+
+
+
+
+
+
 bot.polling(none_stop=True)
